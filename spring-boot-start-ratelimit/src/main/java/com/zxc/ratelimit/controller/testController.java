6d9900d395a280c10e8c443512c245888438716e
zxc705505431 <705505431@qq.com>
2020-04-02 23:41:49 +0800

    commit

---
+++ spring-boot-start-ratelimit/src/main/java/com/zxc/ratelimit/controller/testController.java
@@ -0,0 +1,26 @@
+package com.zxc.ratelimit.controller;
+
+import com.zxc.ratelimit.annotation.AccessLimit;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author zxc
+ * @date 2019/8/21 11:04
+ */
+@Controller
+@RequestMapping("/redis_rate_limit")
+public class testController {
+
+    @ResponseBody
+    @RequestMapping("/seckill")
+    @AccessLimit(limit = 2,sec = 10)  //加上自定义注解即可
+    public String test (HttpServletRequest request, @RequestParam(value = "username",required = false) String userName){
+        //TODO somethings……
+        return   "hello world !";
+    }
+}
